f607e62d6237ed9cbe31f10f472daf733ac37ee3
SeungHan <jk05018@naver.com>
2022-07-03 09:30:41 +0900

    build : spring boot application project build

---
+++ src/main/java/com/jojoldu/book/freelecspringboot/FreelecSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootApplication.class, args);
+	}
+
+}
