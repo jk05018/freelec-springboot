2cb3604895fb86cf20effd3d40f59774f9ac2d4c
SeungHan <jk05018@naver.com>
2022-07-03 15:23:05 +0900

    feat : google 로그인 구현

---
+++ src/main/java/com/jojoldu/book/freelecspringboot/domain/user/User.java
@@ -0,0 +1,56 @@
+package com.jojoldu.book.freelecspringboot.domain.user;
+
+import com.jojoldu.book.freelecspringboot.domain.BaseTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+
+  private String picture;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+  @Builder
+  public User(String name, String email, String picture, Role role) {
+    this.name = name;
+    this.email = email;
+    this.picture = picture;
+    this.role = role;
+  }
+
+  public User update(String name, String picture) {
+    this.name = name;
+    this.picture = picture;
+
+    return this;
+  }
+
+  public String getRoleKey(){
+    return this.role.getKey();
+  }
+}
--- src/main/java/com/jojoldu/book/freelecspringboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/freelecspringboot/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.jojoldu.book.freelecspringboot.web;
 
+import com.jojoldu.book.freelecspringboot.config.dto.SessionUser;
 import com.jojoldu.book.freelecspringboot.services.PostsService;
 import com.jojoldu.book.freelecspringboot.web.dto.PostsResponseDto;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
   private final PostsService postsService;
+  private final HttpSession httpSession;
 
   @GetMapping("/")
   public String index(Model model){
     model.addAttribute("posts", postsService.findAllDesc());
+
+    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+    if (user != null) {
+      model.addAttribute("userName", user.getName());
+    }
+
     return "index";
   }
 
