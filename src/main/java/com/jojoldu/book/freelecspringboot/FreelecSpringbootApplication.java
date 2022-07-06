8cc11576cfc7f9e0c3807ea6e34814e002666b42
SeungHan <jk05018@naver.com>
2022-07-06 22:46:57 +0900

    test 배포 자동화

--- src/main/java/com/jojoldu/book/freelecspringboot/FreelecSpringbootApplication.java
+++ src/main/java/com/jojoldu/book/freelecspringboot/FreelecSpringbootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FreelecSpringbootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FreelecSpringbootApplication.class, args);
 	}
 
