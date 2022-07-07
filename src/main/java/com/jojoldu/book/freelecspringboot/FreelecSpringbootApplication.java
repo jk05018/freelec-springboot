1d857221afb6570d5cd62508bd88e4e6f8a11d84
SeungHan <jk05018@naver.com>
2022-07-07 21:08:09 +0900

    feat: 실제 배포 과정 체험

--- src/main/java/com/jojoldu/book/freelecspringboot/FreelecSpringbootApplication.java
+++ src/main/java/com/jojoldu/book/freelecspringboot/FreelecSpringbootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class FreelecSpringbootApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(FreelecSpringbootApplication.class, args);
